6a68ab9a231fdef19fa6f3b0c20c573922b39264
Michael Coates <popcorndevils@gmail.com>
2021-12-13 08:57:03 -0500

    changed name of main class and added hello world type code.

---
+++ SharpLox.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class SharpLox
+{
+    static void Main(string[] args)
+    {
+        foreach(string a in args)
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
