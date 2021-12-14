6665189c643b06d4e14b3a877f42fec14d9ef09f
Michael Coates <popcorndevils@gmail.com>
2021-12-13 21:41:20 -0500

    Basic REPL

--- SharpLox.cs
+++ SharpLox.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.IO;
 
 public class SharpLox
 {
     static void Main(string[] args)
     {
-        foreach(string a in args)
+        if(args.Count() > 1)
         {
-            Console.WriteLine(a);
+            Console.WriteLine("Usage: SharpLox [script]");
         }
+        else if(args.Count() == 1)
+        {
+            SharpLox.RunFile(args[0]);
+        }
+        else
+        {
+            SharpLox.RunPrompt();
+        }
+    }
+
+    static void RunFile(string path)
+    {
+        string code = File.ReadAllText(path);
+        SharpLox.Run(code);
+    }
+
+    static void RunPrompt()
+    {
+        Console.Write(">> ");
+        string? code = Console.ReadLine();
+        if(code != null && code != "exit()")
+        {
+            SharpLox.Run(code);
+            SharpLox.RunPrompt();
+        }
+    }
+
+    static void Run(string code)
+    {
+        Console.WriteLine(code);
     }
 }
