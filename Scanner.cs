23ad3be5fcb66efdf6dff30335b7980f325529f9
Michael Coates <popcorndevils@gmail.com>
2021-12-28 08:02:01 -0500

    pre actual scanner

---
+++ Scanner.cs
@@ -0,0 +1,8 @@
+
+public static class Scanner
+{
+    public static string[] Tokenize(string source)
+    {
+        return source.Split(' ');
+    }
+}
---
+++ TokenType.cs
@@ -0,0 +1,9 @@
+public enum TokenType
+{
+    // single-character tokens
+    LEFT_PAREN, RIGHT_PARENT, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOG, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    // one-or-two-character tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+}
